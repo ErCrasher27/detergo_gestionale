ca8bd00dee3c33e1771b3d70392065b63fa46fbe
ErCrasher27 <francescograuso@live.it>
2023-01-03 13:18:19 +0100

    backup

--- detergo_gestionale_django/detergo_gestionale_app/admin.py
+++ detergo_gestionale_django/detergo_gestionale_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Articolo)
+admin.site.register(Colore)
+admin.site.register(Difetto)
+admin.site.register(CapoPortato)
+
+
+
+
--- detergo_gestionale_django/detergo_gestionale_app/views.py
+++ detergo_gestionale_django/detergo_gestionale_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import *
+from .serializers import *
+
+
+class LatestClientiList(APIView):
+    def get(self, request, format=None):
+        cliente = Cliente.objects.all()[all]
+        serializers = ClienteSerializer(cliente, many=True)
+        return Response(serializers.data)
