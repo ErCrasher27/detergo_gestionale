87f8e7e9a6353393208743130cad7c5b932b9fd7
ErCrasher27 <francescograuso@live.it>
2023-01-03 12:01:40 +0100

    backup

---
+++ detergo_gestionale_django/detergo_gestionale_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DetergoGestionaleAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'detergo_gestionale_app'
--- detergo_gestionale_vue/src/App.vue
+++ detergo_gestionale_vue/src/App.vue
@@ -6,14 +6,15 @@
         <router-link to="/" class="navbar-item">
           <strong>Detergo Gestionale</strong>
         </router-link>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -42,6 +43,16 @@
 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
