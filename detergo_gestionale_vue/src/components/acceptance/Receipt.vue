290f5e4d5fd1d17d847859603c7560ae8f1d0c8a
ErCrasher27 <francescograuso@live.it>
2023-01-08 17:54:38 +0100

    backup

--- detergo_gestionale_vue/src/components/acceptance/Receipt.vue
+++ detergo_gestionale_vue/src/components/acceptance/Receipt.vue
@@ -1,5 +1,5 @@
 <template>
-    <table class="table">
+    <table class="table is-hoverable">
         <thead>
             <tr>
                 <th>Articolo</th>
@@ -9,12 +9,39 @@
         </thead>
         <tfoot>
             <tr>
-                <th>Tot. pezzi: 3</th>
+                <th>Tot. pezzi: 4</th>
+                <th></th>
+                <th>Tot. Euro: 30</th>
+            </tr>
+            <tr>
+                <th>Cliente: Fabrizia Bucci</th>
+                <th>Data restituzione: 12/01/2005</th>
+                <th>Note: questo scontrino è urgente perchè il cliente deve andare ad una cerimonia</th>
+            </tr>
+            <tr>
+                <th><button class="button">Clienti</button></th>
+                <th><button class="button">Restituzione</button></th>
+                <th><button class="button">note</button></th>
             </tr>
         </tfoot>
         <tbody>
             <tr>
-                <th>Gonna</th>
+                <td>Gonna</td>
+                <td>L1</td>
+                <td>5,00</td>
+            </tr>
+            <tr>
+                <td>Pantalone</td>
+                <td>L2</td>
+                <td>9,00</td>
+            </tr>
+            <tr>
+                <td>Piumone</td>
+                <td>L1</td>
+                <td>16,00</td>
+            </tr>
+            <tr>
+                <td>Maglia</td>
                 <td>L1</td>
                 <td>5,00</td>
             </tr>
